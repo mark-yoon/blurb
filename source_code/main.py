52bb9d1133415a0a966ab515912f1c8ce8972676
Richard Zhang <rmz35@cornell.edu>
2014-02-09 00:32:04 -0500

    wrote function to get five of today's articles

--- source_code/main.py
+++ source_code/main.py
@@ -1,5 +1,9 @@
+import time
+import infoPuller
+
 key = "33FD9081AE0D75A3717CCBBAAABBA66B:10:68797139"
 numArticles = 100
+url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?sort=oldest"
 
 def getArticles(amt):
     articles = []
@@ -14,3 +18,13 @@ def getArticles(amt):
         docs = jsonData["response"]["docs"]
         articles = articles + docs
     return articles
+
+def getTodayArticles():
+    today = (time.strftime("%Y%m%d"))
+    urlcopy = url + "&begin_date=" + today + "&api-key=" + key
+    page = urllib2.urlopen(urlcopy)
+    html = page.read()
+    page.close()
+    jsonData = json.loads(html)
+    docs = jsonData["response"]["docs"][0:5]
+    return docs
